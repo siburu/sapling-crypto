f33e3b85d0fc020e287aeb2c5cac85f50ba5601d
dark64 <darem966@gmail.com>
2019-11-10 19:10:49 +0100

    replace rust-crypto with sha2

--- src/util.rs
+++ src/util.rs
@@ -1,9 +1,8 @@
 use blake2_rfc::blake2b::Blake2b;
 use blake2_rfc::blake2s::Blake2s;
-use crypto::sha2::Sha256;
+use sha2::{Sha256, Digest};
 
 use jubjub::{JubjubEngine, ToUniform};
-
 
 pub fn hash_to_scalar<E: JubjubEngine>(persona: &[u8], a: &[u8], b: &[u8]) -> E::Fs {
     let mut hasher = Blake2b::with_params(64, &[], &[], persona);
@@ -22,12 +21,10 @@ pub fn hash_to_scalar_s<E: JubjubEngine>(persona: &[u8], a: &[u8], b: &[u8]) -> 
 }
 
 pub fn sha256_hash_to_scalar<E: JubjubEngine>(persona: &[u8], a: &[u8], b: &[u8]) -> E::Fs {
-    use crypto::digest::Digest;
     let mut hasher = Sha256::new();
     hasher.input(persona);
     hasher.input(a);
     hasher.input(b);
-    let mut bytes = [0u8; 32];
-    hasher.result(&mut bytes[..]);
-    E::Fs::to_uniform_32(bytes.as_ref())
+    let result = hasher.result();
+    E::Fs::to_uniform_32(result.as_slice())
 }
